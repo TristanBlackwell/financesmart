ec42f969ec05fe208250b934738f42b3b4c4acfb
Tristan Blackwell <tristanblackwell1@gmail.com>
2020-09-16 20:13:03 +0100

    Remove redundant imports and create summary scrolling activity

--- app/src/main/java/com/example/myfirstapp/LongTermGoals.java
+++ app/src/main/java/com/example/myfirstapp/LongTermGoals.java
@@ -2,17 +2,12 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Type;
-import java.util.List;
 
 public class LongTermGoals extends AppCompatActivity {
     public static boolean longTermChecked, longTermAgeChecked;
@@ -52,5 +47,9 @@ public class LongTermGoals extends AppCompatActivity {
         SharedPreferences.Editor editor = store.edit();
         editor.putBoolean("longTermGoals", longTermChecked);
         editor.putBoolean("longTermAge", longTermAgeChecked);
+        editor.apply();
+
+        Intent summary = new Intent(this, Summary.class);
+        startActivity(summary);
     }
 }
