d6dbc9ed2476c514590226c2dd60a44a716007ad
Tristan Blackwell <tristanblackwell1@gmail.com>
2020-09-19 09:49:59 +0100

    add summary views for takehome, outgoing and emefund

--- app/src/main/java/com/example/myfirstapp/Summary.java
+++ app/src/main/java/com/example/myfirstapp/Summary.java
@@ -1,15 +1,11 @@
 package com.example.myfirstapp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import android.widget.TextView;
 
 public class Summary extends AppCompatActivity {
 
@@ -17,18 +13,36 @@ public class Summary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
-        toolBarLayout.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        SharedPreferences store = getApplicationContext().getSharedPreferences("store", MODE_PRIVATE);
+
+        TextView takeHomeSummaryContainer = findViewById(R.id.takeHomeSummaryValue);
+        int takeHomeSummaryValue = store.getInt("takehome", 0);
+        takeHomeSummaryContainer.setText(String.format(getResources().getString(R.string.takeHomeSummary), takeHomeSummaryValue));
+
+        TextView outgoingsSummaryContainer = findViewById(R.id.outgoingsSummaryValue);
+        TextView outgoingsSummaryDescription = findViewById(R.id.outgoingsSummaryDescription);
+        int outgoingsSummaryValue = store.getInt("outgoings", 0);
+        outgoingsSummaryContainer.setText(String.format(getResources().getString(R.string.outgoingsSummary), outgoingsSummaryValue));
+
+        int outgoingsPercentage = (int)(100.0f * outgoingsSummaryValue) / takeHomeSummaryValue;
+        if (outgoingsPercentage > 50) {
+            outgoingsSummaryDescription.setText(String.format(getResources().getString(R.string.outgoingsSummaryDescriptionNegative), outgoingsPercentage));
+        } else {
+            outgoingsSummaryDescription.setText(String.format(getResources().getString(R.string.outgoingsSummaryDescriptionPositive), outgoingsPercentage));
+        }
+
+        TextView emergencyFundSummaryContainer = findViewById(R.id.emergencyFundSummaryValue);
+        TextView emergencyFundSummaryDescription = findViewById(R.id.emergencyFundSummaryDescription);
+        int emergencyFundSummaryValue = store.getInt("emergencyfund", 0);
+        emergencyFundSummaryContainer.setText(String.valueOf(emergencyFundSummaryValue));
+
+        int emergencyFundTarget = outgoingsSummaryValue * 6;
+        if (emergencyFundSummaryValue > emergencyFundTarget) {
+            emergencyFundSummaryDescription.setText(String.format(getResources().getString(R.string.emergencyFundReached), emergencyFundTarget));
+        } else {
+            emergencyFundSummaryDescription.setText(String.format(getResources().getString(R.string.emergencyFundLacking), emergencyFundTarget));
+        }
     }
 }
