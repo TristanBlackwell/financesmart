8aa0aa890ace13b93d551617f7df408bd22a5820
Tristan Blackwell <tristanblackwell1@gmail.com>
2020-09-21 10:34:25 +0100

    add pension summary view and code

--- app/src/main/java/com/example/myfirstapp/Summary.java
+++ app/src/main/java/com/example/myfirstapp/Summary.java
@@ -17,10 +17,13 @@ public class Summary extends AppCompatActivity {
 
         SharedPreferences store = getApplicationContext().getSharedPreferences("store", MODE_PRIVATE);
 
+        // Display take home entered previously
         TextView takeHomeSummaryContainer = findViewById(R.id.takeHomeSummaryValue);
         int takeHomeSummaryValue = store.getInt("takehome", 0);
         takeHomeSummaryContainer.setText(String.format(getResources().getString(R.string.takeHomeSummary), takeHomeSummaryValue));
 
+        // Display outgoings entered previously. If outgoings are greater than 50% of income suggest
+        // user to cut costs otherwise display good message.
         TextView outgoingsSummaryContainer = findViewById(R.id.outgoingsSummaryValue);
         TextView outgoingsSummaryDescription = findViewById(R.id.outgoingsSummaryDescription);
         int outgoingsSummaryValue = store.getInt("outgoings", 0);
@@ -33,6 +36,8 @@ public class Summary extends AppCompatActivity {
             outgoingsSummaryDescription.setText(String.format(getResources().getString(R.string.outgoingsSummaryDescriptionPositive), outgoingsPercentage));
         }
 
+        /* Display emergency fund entered previously. if user hasn't saved 6 months worth of their monthly
+        outgoings suggest an increase in fund otherwise keep said fund in an easy access account. */
         TextView emergencyFundSummaryContainer = findViewById(R.id.emergencyFundSummaryValue);
         TextView emergencyFundSummaryDescription = findViewById(R.id.emergencyFundSummaryDescription);
         int emergencyFundSummaryValue = store.getInt("emergencyfund", 0);
@@ -43,6 +48,16 @@ public class Summary extends AppCompatActivity {
             emergencyFundSummaryDescription.setText(String.format(getResources().getString(R.string.emergencyFundReached), emergencyFundTarget));
         } else {
             emergencyFundSummaryDescription.setText(String.format(getResources().getString(R.string.emergencyFundLacking), emergencyFundTarget));
+        }
+
+        // Suggest to user to find out more about their workplace pension if not contributing max
+        // otherwise tell user to maintain contributions
+        TextView pensionSummaryDescription = findViewById(R.id.pensionSummaryDescription);
+        boolean pensionMax = store.getBoolean("pensionMax", false);
+        if (pensionMax) {
+            pensionSummaryDescription.setText(getResources().getString(R.string.pensionSummaryGood));
+        } else {
+            pensionSummaryDescription.setText(getResources().getString(R.string.pensionSummaryBad));
         }
     }
 }
